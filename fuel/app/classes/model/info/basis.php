53fa95c5d9bd836ed033609ff90c4550450f4e62
mtoksuy <mtoksuy@programmerbox.com>
2018-07-17 18:53:57 +0900

    version_0.1 infoモデル作成

---
+++ fuel/app/classes/model/info/basis.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+* 
+* インフォ基本クラス
+* 
+* 
+* 
+*/
+class Model_Info_Basis {
+	//----------------------------------
+	//登録済みのメールアドレスかチェック
+	//----------------------------------
+	public static function mail_address_check($mail) {
+		var_dump($mail);
+	}
+
+
+
+}
