a3606e766f8fafef3cb04828ffe24be2c371e957
mtoksuy <mtoksuy@programmerbox.com>
2018-10-19 16:55:08 +0900

    develop_0.3 flatpickr起動

--- fuel/app/views/salesfllow/sales/script.php
+++ fuel/app/views/salesfllow/sales/script.php
@@ -2,10 +2,8 @@
 			<!-- jQueryプラグイン -->
 			<script type="text/javascript" src="<?php echo HTTP; ?>assets/js/library/jquery-1.9.1-min.js"></script>
 			<!-- flatpickrプラグイン -->
-<!--
 			<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 			<script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/ja.js"></script>
--->
 			<!-- 日本語の言語ファイル -->
 			<script src="//unpkg.com/flatpickr/dist/l10n/ja.js"></script>
 			<!-- ジェネラルプラグイン -->
