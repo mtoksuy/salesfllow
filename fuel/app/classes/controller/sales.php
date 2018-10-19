1b4905ab95f62ef281e47fc8b5ddb8d4e2c880ef
mtoksuy <mtoksuy@programmerbox.com>
2018-10-19 16:13:21 +0900

    develop_0.3 セールスノート実装

--- fuel/app/classes/controller/sales.php
+++ fuel/app/classes/controller/sales.php
@@ -35,10 +35,15 @@ class Controller_Sales extends Controller_Sales_Template {
 		if($method == 'index') {
 			return $this->action_index($post);
 		}
+			// ノート詳細ページ
+			else if($method == 'note' && $params[0]) {
+				$note_node_primary_id = (int)$params[0];
+				return $this->action_note($note_node_primary_id);
+			}
 			// 案件詳細ページ
 			else if($params == false) {
 				// 数字を圧縮する
-				$encode = Library_Shorturl_Basis::shot_url_encode(777);
+//				$encode = Library_Shorturl_Basis::shot_url_encode(777);
 				// 圧縮した数字(文字列)を数字に戻す
 				$decode           = Library_Shorturl_Basis::shot_url_decode($method);
 				$sales_primary_id = $decode;
@@ -67,16 +72,11 @@ class Controller_Sales extends Controller_Sales_Template {
 			// 案件作成
 			Model_Sales_Basis::sales_create($post);
 		}
-
-
-
-
-
-		$time_start = microtime(true);
-		// 
-		$folder_html = Model_Sales_Basis::sales_nest_type_3_list_get((int)$_COOKIE['user_data']['user_primary_id']);
-		$time = microtime(true) - $time_start;
-//		echo "{$time} 秒";
+		// シンプル型の案件フォルダリスト取得
+		$note_node_res = Model_Sales_Basis::sales_type_simple_folder_list_res_get((int)$_COOKIE['user_data']['user_primary_id']);
+		// セールスフォルダーHTML生成
+		$folder_html = Model_Sales_Html::sales_folder_html_create($note_node_res, 'root');
+		// ゴミ箱(使ってないようだ
 		$note_trash_list_res = Model_Sales_Basis::note_trash_list_get((int)$_COOKIE['user_data']['user_primary_id']);
 
 
@@ -101,24 +101,62 @@ class Controller_Sales extends Controller_Sales_Template {
 			'sales_list_html'  => $sales_list_html,
 		), false);
 	}
+	///////////////////
+	// ノートアクション
+	///////////////////
+	public function action_note($note_node_primary_id) {
+		// シンプル型の案件フォルダリストres取得
+		$note_node_res = Model_Sales_Basis::sales_type_simple_folder_list_res_get((int)$_COOKIE['user_data']['user_primary_id']);
+		// セールスフォルダーHTML生成
+		$folder_html = Model_Sales_Html::sales_folder_html_create($note_node_res, $note_node_primary_id);
+
+		// コンテンツデータセット
+		$this->sales_template->view_data["content"]->set('content_data', array(
+			'folder_html'     => View::forge('salesfllow/sales/folder'),
+			'sales_list_html' => View::forge('salesfllow/sales/list'),
+			'content_html'    => View::forge('salesfllow/sales/salescreate'),
+		), false);
+		// コンテンツデータセット
+		$this->sales_template->view_data["content"]->content_data['folder_html']->set('content_data', array(
+			'sales_now'   => 'now',
+			'folder_html' => $folder_html,
+		), false);
+
+		// 案件リスト取得
+		$sales_note_res = Model_Sales_Basis::sales_note_list_get($_COOKIE['user_data']['user_primary_id'], $note_node_primary_id);
+		// 案件リストHTML生成
+		$sales_list_html = Model_Sales_Html::sales_list_html_create($sales_note_res);
+		// コンテンツデータセット
+		$this->sales_template->view_data["content"]->content_data['sales_list_html']->set('content_data', array(
+			'sales_list_html'  => $sales_list_html,
+		), false);
+	}
 	/////////////////////
 	// 案件詳細アクション
 	/////////////////////
 	public function action_sales($sales_primary_id) {
+		// 
+		Model_Sales_Basis::a();
+		// シンプル型の案件フォルダリスト取得
+		$note_node_res = Model_Sales_Basis::sales_type_simple_folder_list_res_get((int)$_COOKIE['user_data']['user_primary_id']);
+		// セールスフォルダーHTML生成
+		$folder_html = Model_Sales_Html::sales_folder_html_create($note_node_res);
+
 		// セールスresを取得
 		$sales_res = Model_Sales_Basis::sales_res_get($sales_primary_id);
 		// セールスHTML生成
 		$sales_html = Model_Sales_Html::sales_html_create($sales_res);
+
 		// コンテンツデータセット
 		$this->sales_template->view_data["content"]->set('content_data', array(
-			'function_html'   => View::forge('salesfllow/function'),
+			'folder_html'   => View::forge('salesfllow/sales/folder'),
 			'sales_list_html' => View::forge('salesfllow/sales/list'),
 			'content_html'    => $sales_html,
 		), false);
-
 		// コンテンツデータセット
-		$this->sales_template->view_data["content"]->content_data['function_html']->set('content_data', array(
+		$this->sales_template->view_data["content"]->content_data['folder_html']->set('content_data', array(
 			'sales_now'  => 'now',
+			'folder_html' => $folder_html,
 		), false);
 
 		//案件リスト取得
@@ -130,20 +168,6 @@ class Controller_Sales extends Controller_Sales_Template {
 			'sales_list_html'  => $sales_list_html,
 		), false);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
